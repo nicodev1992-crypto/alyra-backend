3b4c1956cd6f1da75e0951140ed4d7345a68c385
nicodev1992-crypto <nicodev1992@gmail.com>
2026-05-09 11:04:32 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ class GlucoseCreate(BaseModel):
 def add_glucose(data: GlucoseCreate, db=Depends(get_db)):
     query = text("""
         INSERT INTO glucose_measurements 
-        (user_id, glucose_value, phase, source_type, measured_at)
+        (user_id, sugar_value, phase, source_type, measured_at)
         VALUES (:u_id, :g_val, :ph, :s_type, :m_at)
     """)
     
