9e6009fedc694a1fa56575943c2f6b7b747ed375
nicodev1992-crypto <nicodev1992@gmail.com>
2026-05-14 16:20:39 +0200

    Add files via upload

--- post.py
+++ post.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix="/post")
 
 
 # -------------------------------------- USER REGISTER AND LOGIN
-@router.post("/insert/register_user")
+@router.post("/register_user")
 def insert_new_profile(user: schemas.UserRegister, db=Depends(get_db)):
     logger.info(f"Inserimento profilo completo per: {user.full_name}")
 
